5aa5580b1325b4677ca7c112db248f463f5f7b2d
wasabi315 <wasabi.nvim.st@gmail.com>
2024-03-20 00:20:07 +0900

    Update

--- 6-tail-call.js
+++ 6-tail-call.js
@@ -25,7 +25,7 @@ function Call(fun, ...args) {
         return Eval(fun(...args));
       }
       if (arity > nargs) {
-        const pap = (...args2) => App(Fun(fun), ...args, ...args2);
+        const pap = (...args2) => Fun(fun)(...args, ...args2);
         Object.defineProperty(pap, "length", { value: arity - nargs });
         return Eval(Fun(pap));
       }
@@ -107,14 +107,15 @@ function ReturnInt(n) {
 }
 
 export function Thunk(expr) {
-  const self = {
-    eval(stacks) {
-      self.eval = Blackhole.eval;
-      stacks.upd.push({ target: self, ret: stacks.ret, call: stacks.call });
-      stacks.call = [];
-      stacks.ret = [];
-      return Eval(expr());
-    },
+  function self(...args) {
+    return App(self, ...args);
+  }
+  self.eval = (stacks) => {
+    self.eval = Blackhole.eval;
+    stacks.upd.push({ target: self, ret: stacks.ret, call: stacks.call });
+    stacks.call = [];
+    stacks.ret = [];
+    return Eval(expr());
   };
   return self;
 }
@@ -144,11 +145,11 @@ export function App(fun, ...args) {
 }
 
 export function Fun(fun) {
-  return {
-    eval(_stacks) {
-      return ReturnFun(fun);
-    },
-  };
+  function self(...args) {
+    return App(self, ...args);
+  }
+  self.eval = (_stacks) => ReturnFun(fun);
+  return self;
 }
 
 export function Con(con, ...args) {
@@ -169,101 +170,85 @@ export function Int(n) {
 
 const add = Fun((x, y) => {
   return Case(x, {
-    default: (n) => {
-      return Case(y, {
+    default: (n) =>
+      Case(y, {
         default: (m) => Int(n + m),
-      });
-    },
+      }),
   });
 });
 const sub = Fun((x, y) => {
   return Case(x, {
-    default: (n) => {
-      return Case(y, {
+    default: (n) =>
+      Case(y, {
         default: (m) => Int(n - m),
-      });
-    },
+      }),
   });
 });
 
 const Cons = (x, xs) => Con("Cons", x, xs);
 const Nil = Con("Nil");
-const tail = Fun((xs) => {
-  return Case(xs, {
-    Cons: (_, xs) => xs,
-  });
-});
+const tail = Fun((xs) => Case(xs, { Cons: (_, xs) => xs }));
 const take = Fun((n, xs) => {
   return Case(n, {
     [0]: () => Nil,
-    default: () => {
-      return Case(xs, {
+    default: () =>
+      Case(xs, {
         Nil: () => Nil,
         Cons: (x, xs) => {
-          const m = Thunk(() => App(sub, n, Int(1)));
-          const tmxs = Thunk(() => App(take, m, xs));
+          const m = Thunk(() => sub(n, Int(1)));
+          const tmxs = Thunk(() => take(m, xs));
           return Cons(x, tmxs);
         },
-      });
-    },
+      }),
   });
 });
 const zipWith = Fun((f, xs, ys) => {
   return Case(xs, {
     Nil: () => Nil,
-    Cons: (x, xs) => {
-      return Case(ys, {
+    Cons: (x, xs) =>
+      Case(ys, {
         Nil: () => Nil,
         Cons: (y, ys) => {
-          const fxy = Thunk(() => App(f, x, y));
-          const zfxsys = Thunk(() => App(zipWith, f, xs, ys));
+          const fxy = Thunk(() => f(x, y));
+          const zfxsys = Thunk(() => zipWith(f, xs, ys));
           return Cons(fxy, zfxsys);
         },
-      });
-    },
+      }),
   });
 });
 
 const Unit = Con("Unit");
-const then = Fun((x, y) => {
-  return Case(x, {
-    default: () => {
-      return Case(y, {
-        default: (y) => y,
-      });
-    },
-  });
-});
-const traverse_ = Fun((f, xs) => {
-  return Case(xs, {
+const seq = Fun((x, y) => Case(x, { default: () => y }));
+const traverse_ = Fun((f, xs) =>
+  Case(xs, {
     Nil: () => Unit,
     Cons: (x, xs) => {
-      const fx = Thunk(() => App(f, x));
-      const tfxs = Thunk(() => App(traverse_, f, xs));
-      return App(then, fx, tfxs);
+      const fx = Thunk(() => f(x));
+      const tfxs = Thunk(() => traverse_(f, xs));
+      return seq(fx, tfxs);
     },
-  });
-});
-const printInt = Fun((x) => {
-  return Case(x, {
+  })
+);
+const printInt = Fun((x) =>
+  Case(x, {
     default: (n) => {
       console.log(n);
       return Unit;
     },
-  });
-});
+  })
+);
 
 const fibs = Thunk(() => {
-  const xs = Thunk(() => App(tail, fibs));
-  const ys = Thunk(() => App(zipWith, add, fibs, xs));
+  const xs = Thunk(() => tail(fibs));
+  const ys = Thunk(() => zipWith(add, fibs, xs));
   const zs = Thunk(() => Cons(Int(1), ys));
   return Cons(Int(0), zs);
 });
 
 const main = Thunk(() => {
   const n = Thunk(() => Int(100));
-  const fs = Thunk(() => App(take, n, fibs));
-  return App(traverse_, printInt, fs);
+  const fs = Thunk(() => take(n, fibs));
+  return traverse_(printInt, fs);
 });
 
 Evaluate(main);
